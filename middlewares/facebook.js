07533f16354cd5c77a2c26a629ab0b1ee9138fc4
balo11044 <balo11044@gmail.com>
2018-11-20 14:24:11 +0700

    'ok'

--- middlewares/facebook.js
+++ middlewares/facebook.js
@@ -2,10 +2,24 @@ const axios = require('axios');
 
 const facebookConfig = require('../config')
 
-function getUserInfo(req, res) {
-    axios.get(`https://graph.facebook.com/v2.5/me?fields=id,name,birthday,gender,email&access_token=${facebookConfig.ACCESS_TOKEN}`)
+const facebookApi = `https://graph.facebook.com/v3.2`;
+const accessToken = `access_token=${facebookConfig.ACCESS_TOKEN}`;
+const facebookId = '2160780914184653'
+
+function getUserInfo(req, res, next) {
+    axios.get(`${facebookApi}/${facebookId }?fields=id,name,birthday,gender,email&${accessToken}`)
         .then(data => console.log(data.data))
         .catch(err => console.log(err))
+    next();
+}
+//THAM SO DAU LA ? CAC THAM SO SAU LA &
+// EAAIT0HzWdfUBAPumviwWV2MxX4KCNeQQEo4qcdw3aeuv50u25LxRLe2dvZBlqG9OZAkDEfRPAD8uku079W0EudeV7i3rLooLVLKVjnjtVwG4N8hk5UxS0UVFUlOa16GNCPen3ZCkCdZCOsObxbodnp1gf6DOeVgV5erAQOGaa1VrZCCx8Jjnhld7wDmpgsvpcIof6X8FKlQZDZD
+
+function getThings(req, res, next){
+    axios.get(`${facebookApi}/${facebookId}/feed?${accessToken}`)
+    .then(data => console.log(data.data))
+    .catch(err => console.log(err))
+next()
 }
 
 function checkIn(req, res, next) {
@@ -15,5 +29,7 @@ function checkIn(req, res, next) {
 
 module.exports = {
     getUserInfo,
-    checkIn
+    checkIn,
+    getThings
 }
+
--- routes/facebook.js
+++ routes/facebook.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const facebookHelper = require('../middlewares/facebook');
 
 // router.use('/', facebookHelper.checkIn);
-router.get('/', facebookHelper.getUserInfo);
+router.get('/userInfo', facebookHelper.getUserInfo, (req, res) => res.send('info logged'));
+router.get('/getThings', facebookHelper.getThings, (req, res) => res.send('photos logged'));
 
 module.exports = router;
